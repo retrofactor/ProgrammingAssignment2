d4c4680fe100861986a702566eec5df1e79b8f56
retrofactor <retrofactor@gmail.com>
2014-06-14 16:49:42 +0200

    makeCacheMatrix and cacheSolve implementations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix(x) 
+##  Creates a cache for a matrix x and its inverse.
+##
+##  Returns a list of four functions that are used to access and modify the cached matrices:
+##    get() - that retrieves the cached matrix
+##    set(y) - that replaces the matirx in cache with the matirx y
+##    getinv() - that retrieves the inverse of the cached matrix
+##    setinv(inv) - that replaces the inverse of the cached matrix with the matrix inv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL # placeholder for the inverse of x
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<-inv
+    getinv <- function() m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve(x)
+##  Computes the inverse of a "matrix" x (which has to be constructed 
+##  using makeCacheMatrix function).
+##  Returns the cached inverse if it has already been computed by the previous 
+##  cacheSolve call.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        # getting cached data
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
